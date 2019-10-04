f4c0c17caa865967445704036f34a1f7352dc466
Marc Charmois <marc.charmois@hotmail.com>
2019-10-04 19:08:50 +0200

    initial commit

---
+++ events-listener.py
@@ -0,0 +1,53 @@
+import logging
+from azure.eventhub import EventHubClient, EventData, EventPosition
+import config as cfg
+
+def getEvents(lastOffset):
+    try:
+        client = EventHubClient.from_connection_string(cfg.connection_str)
+        EVENT_POSITION = EventPosition(lastOffset)
+        consumer = client.create_consumer(consumer_group="$default", partition_id=PARTITION, event_position=EVENT_POSITION, prefetch=100000)
+        total=0
+        last_offset = -1
+        with consumer:
+            batched_events = consumer.receive(max_batch_size=100000)
+            for event_data in batched_events:
+                last_offset = event_data.offset
+                last_sn = event_data.sequence_number
+                total += 1
+                print("Partition {}, Received {}, sn={} offset={}".format(
+                    PARTITION,
+                    total,
+                    last_sn,
+                    last_offset))
+                print (event_data)
+        getEvents(last_offset)
+        client.stop()
+    except:
+        print('got all events for now')
+    finally:
+        print('got all events for now')
+        client.stop()
+
+#EVENT_POSITION = EventPosition("12500000")
+EVENT_POSITION = EventPosition("-1")
+PARTITION = "0"
+client = EventHubClient.from_connection_string(cfg.connection_str)
+consumer = client.create_consumer(consumer_group="$default", partition_id=PARTITION, event_position=EVENT_POSITION, prefetch=100000)
+total=0
+last_offset = -1
+with consumer:
+    batched_events = consumer.receive(max_batch_size=100000)
+    for event_data in batched_events:
+        last_offset = event_data.offset
+        last_sn = event_data.sequence_number
+        total += 1
+        print("Partition {}, Received {}, sn={} offset={}".format(
+            PARTITION,
+            total,
+            last_sn,
+            last_offset))
+        print (event_data)
+getEvents(last_offset)
+client.stop()
+
